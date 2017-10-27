c4cec56f96a08c73f1123a9405d42902c181d3e6
Mohamed-Anwar-Aly <anwaraly_pro@yahoo.com>
2017-10-27 04:20:50 +0200

    done!

--- designs.js
+++ designs.js
@@ -1,10 +1,25 @@
-// Select color input
-// Select size input
-
+let color = "#000";
+//Select color input 
+let rowVal, colVal;
+//Select size input
+$("#sizePicker :submit").on("click", function(evt) {
+    evt.preventDefault();
+    makeGrid();
+});
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
-
-// Your code goes here!
-
+    let body = $("#pixel_canvas")[0];
+    rowVal = $("#input_height").val();
+    colVal = $("#input_width").val();
+    for (let r = 0; r < rowVal; r++) {
+        let row = body.insertRow(r);
+        for (let c = 0; c < colVal; c++) {
+            let cell = row.insertCell(c);
+            $(cell).on('click', function(evt) {
+                evt.target.style.backgroundColor = $("#colorPicker").val();
+                this.style.borderColor = "#000";
+            });
+        }
+    }
+    return false;
 }
